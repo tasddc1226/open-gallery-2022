73a8ae8226eb28c2ef7190ca4deccd8a7cd045dd
tasddc1226 <tasddc@naver.com>
2022-10-22 21:37:14 +0900

    Fix: get SECRET key

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
